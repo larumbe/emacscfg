6da9c86bfd9aa12695e174cf2cfa23a9372f1857
Hadrianus Larunbe <adrianml@alumnos.upm.es>
2019-08-19 15:52:54 +0100

    Adds package initialisation

--- init.el
+++ init.el
@@ -1,2 +1,27 @@
 ;;; New config file after .emacs bankruptcy
 
+;;; package initialisation
+(package-initialize)
+
+;;; package sources
+(unless (assoc-default "melpa" package-archives)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
+(unless (assoc-default "org" package-archives)
+  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t))
+
+(add-to-list 'load-path "~/~/usr/src/emacs/dotemacs/elisp/")
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(setq use-package-verbose t)
+(setq use-package-always-ensure t)
+
+(require 'use-package)
+(use-package auto-compile
+  :config (auto-compile-on-load-mode))
+
+(setq load-prefer-newer t)
+
+(global-set-key (kbd "C-x M-p") 'package-list-packages)
