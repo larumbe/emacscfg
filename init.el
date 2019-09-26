ec5ef0df6fe51384d96dee1cd3a4ec17483fa57a
Hadrianus Larunbe <adrianml@alumnos.upm.es>
2019-09-26 15:39:16 +0100

    fixes projectile, helm, moves some functions around

--- init.el
+++ init.el
@@ -9,9 +9,12 @@
 
 (setq-default
  load-prefer-newer t
- package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+ package-enable-at-startup nil
+ package-user-dir (concat default-directory "elpa"))
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+
 (package-initialize)
 
 ;;; Dependencies
@@ -19,11 +22,10 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package t))
+
 (setq-default
  use-package-always-defer t
- use-package-always-ensure t
- package-user-dir (concat default-directory "elpa")
- )
+ use-package-always-ensure t)
 
 (org-babel-load-file (expand-file-name "dotemacs.org"))
 
